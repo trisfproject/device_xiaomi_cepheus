40bc2d6926ed044da3c159fba3f4a8dead4583d1
TRISF <trisfaizal@gmail.com>
2021-04-30 22:59:17 +0700

    cepheus: nad-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_cepheus.mk
+    $(LOCAL_DIR)/nad_cepheus.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_cepheus-user \
-    aosp_cepheus-userdebug \
-    aosp_cepheus-eng
+    nad_cepheus-user \
+    nad_cepheus-userdebug \
+    nad_cepheus-eng
