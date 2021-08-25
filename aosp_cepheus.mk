199ac65c625374fa9d8d922520c0d6d803934f2b
trisf <trisfaizal@gmail.com>
2021-08-25 15:11:38 +0700

    cepheus: inherit Gcam Parrot

--- aosp_cepheus.mk
+++ aosp_cepheus.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/xiaomi/cepheus/device.mk)
 # Inherit some common PE stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+# Google Camera
+$(call inherit-product-if-exists, vendor/apps/GoogleCamera/GoogleCameraParrot.mk)
+
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
